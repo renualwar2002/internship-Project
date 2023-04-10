7b96cb5ab37948ecef7b58db030701d93a0bd94b
lekhacse <lekhacse2020@gmail.com>
2023-04-10 18:40:28 +0530

    au113220031068
    I have deleted previous file to get this ready so you can't find the list activity related to student details here
    I don't know how to add a new project here so I edited the previous files
    But I added the outputs
    Kindly evaluate my project

--- app/src/main/java/com/example/internship/MainActivity.java
+++ app/src/main/java/com/example/internship/MainActivity.java
@@ -1,59 +1,50 @@
 package com.example.internship;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView listview;
-    List<String> listViewValues;
-    ArrayList<String> listviewdetails = new ArrayList<>();
+    ImageView image1;
+    Button button1,button2,button3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listview=(ListView) findViewById(R.id.mobile_list);
+        image1=findViewById(R.id.img);
+        button1=findViewById(R.id.b1);
+        button2=findViewById(R.id.b2);
+        button3=findViewById(R.id.b3);
 
-        try {
-            JSONObject jsonObject=new JSONObject(loadJsonFile());
-            JSONArray jsonArray=jsonObject.getJSONArray("studentdetails");
-            listViewValues = new ArrayList<String>();
-            for(int i=0; i<jsonArray.length();i++)
-            {
-                JSONObject obj=jsonArray.getJSONObject(i);
-                String name=obj.getString("name");
-                listViewValues.add(name);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
             }
-            ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.displayname, listViewValues);
-            listview.setAdapter(adapter);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+        });
 
-    public String loadJsonFile() throws IOException {
-        String json=null;
-        InputStream inputStream=this.getAssets().open("studentdetails.json");
-        int size=inputStream.available();
-        byte[] byteArray=new byte[size];
-        inputStream.read(byteArray);
-        inputStream.close();
-        json=new String(byteArray, "UTF-8");
-        return json;
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity3.class);
+                startActivity(intent);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity4.class);
+                startActivity(intent);
+            }
+        });
     }
 }
